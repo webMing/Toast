8cdd3f5c5c88c4f17787d2d803823a10fd40d124
= <=>
2018-05-22 20:54:10 +0800

    Initial Commit

---
+++ Toast/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Toast
+//
+//  Created by Stephanie on 2018/5/22.
+//  Copyright © 2018年 Stephanie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Toast/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Toast
+//
+//  Created by Stephanie on 2018/5/22.
+//  Copyright © 2018年 Stephanie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
